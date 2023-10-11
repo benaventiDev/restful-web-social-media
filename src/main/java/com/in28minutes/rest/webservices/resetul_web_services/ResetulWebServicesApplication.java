027322d13591e3b65ec906a77b12c300cc0b36fa
benaventi <201021212@ingenieria.usac.edu.gt>
2023-10-11 05:41:42 -0600

    first commit

---
+++ src/main/java/com/in28minutes/rest/webservices/resetul_web_services/ResetulWebServicesApplication.java
@@ -0,0 +1,18 @@
+package com.in28minutes.rest.webservices.resetul_web_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@SpringBootApplication
+public class ResetulWebServicesApplication {
+	
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ResetulWebServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/in28minutes/rest/webservices/resetul_web_services/ResetulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.rest.webservices.resetul_web_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResetulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
