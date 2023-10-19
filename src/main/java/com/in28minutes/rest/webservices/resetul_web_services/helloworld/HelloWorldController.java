78cde2adef6eaa08ac502f797c8a1065cbef5b6f
benaventi <201021212@ingenieria.usac.edu.gt>
2023-10-18 18:03:09 -0600

    Add user functionality

--- src/main/java/com/in28minutes/rest/webservices/resetul_web_services/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/resetul_web_services/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.in28minutes.rest.webservices.resetul_web_services.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -17,6 +18,12 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean HelloWorldBean() {
 		return new HelloWorldBean("Hello World");
+	}
+	
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean HelloWorldPathVariable(@PathVariable String name ) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
+		
 	}
 	
 	
